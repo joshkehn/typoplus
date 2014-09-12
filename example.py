2bf57653ab7d95a55d748ed6b1105a162dfa0481
Joshua Kehn <josh.kehn@gmail.com>
2014-09-12 17:11:58 -0400

    Move to preprocessor

--- example.py
+++ example.py
@@ -1,4 +1,6 @@
-# -*- coding: utf-8 -*- #
+# coding=utf-8
+
+from __future__ import unicode_literals
 import markdown
 from . import typos
 
@@ -8,13 +10,17 @@ md = markdown.Markdown(extensions=[typeit, "extra"])
 text = md.convert("""\
 This is some text.
 
-This is some text with 100x100px dimensions.
+“This is some text with 100x100px dimensions.”
+
+This—I mean this—is an em-dash.
 
 100x100 or 10x10 or 99x10 or 100dpi x 320dpi.
 
 Multiple different types of comparisons can be used such as 10 altoids x 20 phones.
 
-There are, of course, limitations here where a number (10) x (1) doesn't match. The system assumes numeric-typed suffixes.
+That’s ‘all’!
+
+There are, of course, limitations here where a number (10) x (1) doesn't match.
 """)
 
 if __name__ == "__main__":
--- typos.py
+++ typos.py
@@ -3,32 +3,52 @@
 from __future__ import unicode_literals
 import re
 import markdown
-from markdown.postprocessors import Postprocessor
+from markdown.preprocessors import Preprocessor
 
 EXTENSIONS = (
     ("multi", r'(?P<dim1>\b\d+\s*[\w.]*\s*)(x)((?P<dim2>\s*\d+\s*[\w.]*\b))', '&times;'),
-    ("emdash", r'(?P<dim1>\w+)—(?P<dim2>\w+)', '&#x200A;&mdash;&#x200A;'),
-    ("endash", r'(?P<dim1>\w+)–(?P<dim2>\w+)', '&ndash;')
 )
 
+REPLACEMENTS = (
+    ("emdash", r"—", "&#x200A;&mdash;&#x200A;"),
+    ("endash", r"–", "&ndash;"),
+    ("times", r"×", "&times;"),
+    ("ldquo", r"“", "&ldquo;"),
+    ("rdquo", r"”", "&rdquo;"),
+    ("lsquo", r"‘", "&lsquo;"),
+    ("rsquo", r"’", "&rsquo;"),
+)
 
-class TypoSubPost (Postprocessor):
+class SubProcess (Preprocessor):
+
+    def process_line (self, text):
+        return self.pattern.sub(self.repl, text)
+
+    def run (self, lines):
+        return [ self.process_line(line) for line in lines ]
+
+class TypoSubPost (SubProcess):
 
     def __init__ (self, *args, **kwargs):
         self.pattern = re.compile(kwargs.pop("pattern", None))
         self.repl = "\g<dim1>{}\g<dim2>".format(kwargs.pop("repl", None))
         return super(TypoSubPost, self).__init__(*args, **kwargs)
 
-    def run (self, text):
-        return self.pattern.sub(self.repl, text)
+class StraightSubPost (SubProcess):
 
+    def __init__ (self, *args, **kwargs):
+        self.pattern = re.compile(kwargs.pop("pattern", None))
+        self.repl = kwargs.pop("repl")
+        return super(StraightSubPost, self).__init__(*args, **kwargs)
 
 class TypoPlusExtension (markdown.extensions.Extension):
     """Adds a variety of typographic enhancements to Markdown."""
 
     def extendMarkdown (self, md, glob):
         for name, pattern, repl in EXTENSIONS:
-            md.postprocessors.add(name, TypoSubPost(pattern=pattern, repl=repl), "_end")
+            md.preprocessors.add(name, TypoSubPost(pattern=pattern, repl=repl), "_end")
+        for name, pattern, repl in REPLACEMENTS:
+            md.preprocessors.add(name, StraightSubPost(pattern=pattern, repl=repl), "_end")
 
 
 def makeExtension(configs={}):
